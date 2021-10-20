b8ab15f05050a040ea9a15ce51519ce9b4994149
Atul Kumar <atul80100@gmail.com>
2021-10-20 12:43:28 +0530

    End of section 3

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+//using Core.Entities.OrderAggregate;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brand = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+                    foreach (var item in brand)
+                    {
+                        context.ProductBrands.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+                if (!context.ProductTypes.Any())
+                {
+                    var typeData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+                    foreach (var item in types)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+                if (!context.Products.Any())
+                {
+                    var productData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                    foreach (var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+                    await context.SaveChangesAsync();
+                }
+                // if (!context.DeliveryMethodes.Any())
+                // {
+                //     var dmData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                //     var method = JsonSerializer.Deserialize<List<DeliveryMethode>>(dmData);
+                //     foreach (var item in method)
+                //     {
+                //         context.DeliveryMethodes.Add(item);
+                //     }
+                //     await context.SaveChangesAsync();
+                // }
+            }
+            catch (Exception ex)
+            {
+                var logger=loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError("An error in seeding the data!",ex.Message);
+            }
+        }
+    }
+}
